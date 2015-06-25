767340ec3f58c59b5c91765950c8b2e6dafb282e
Peter Schwarz <peter.schwarz@bicycle.io>
2015-06-25 13:19:38 -0500

    Updated deps

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/peterschwarz/partsbox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-firmata "2.0.1"]])
+  :dependencies [[org.clojure/clojure "1.7.0-RC2"]
+                 [clj-firmata "2.1.1-SNAPSHOT"]])
